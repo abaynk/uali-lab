fa7998d216ad39568e05f8bbbe6652e948a3bd86
Abay Nurpeissov <abaynurpeissov@iMac-pk.local>
2024-03-07 14:35:44 +0500

    add animation

--- src/app/(site)/@modal/(.)work/[id]/page.tsx
+++ src/app/(site)/@modal/(.)work/[id]/page.tsx
@@ -4,12 +4,17 @@ import { WorkModal } from "@/components/WorkModal";
 import IProject from "@/types/ProjectType";
 import React, { useEffect, useState } from "react";
 import { getOneProject } from "../../../../../../sanity/lib/query";
+import { AnimatePresence } from "framer-motion";
+import { useRouter } from "next/navigation";
 
 type Props = {};
 
 const ProjectPage = ({ params }: { params: { id: string } }) => {
   const [projectData, setProjectData] = useState<IProject>();
+  const [modalOpen, setModalOpen] = useState(false);
+  const router = useRouter();
   useEffect(() => {
+    setModalOpen(true);
     const fetchProjectData = async (id: string) => {
       const data = await getOneProject(params.id);
       data && setProjectData(data);
@@ -17,7 +22,20 @@ const ProjectPage = ({ params }: { params: { id: string } }) => {
     params.id && fetchProjectData(params.id);
   }, [params.id]);
 
-  return <WorkModal projectData={projectData} />;
+  return (
+    <AnimatePresence
+      initial={false}
+      mode={"wait"}
+      onExitComplete={() => router.back()}
+    >
+      {modalOpen && (
+        <WorkModal
+          projectData={projectData}
+          handleClose={() => setModalOpen(false)}
+        />
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default ProjectPage;
